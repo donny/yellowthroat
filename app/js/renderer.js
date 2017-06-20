c013264691bfa5b323d36a9c91795b0683237915
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-06-21 08:25:23 +1000

    Add initial Reddit subs

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,5 +1,24 @@
-function getData() {
-      console.log('HELLO');
-}
 
-document.querySelector('#btnEd').addEventListener('click', getData)
+$(document).ready(function() {
+
+  $("#worldnews").click(function() {
+    console.log('worldnews');
+    $("#content").html("<p>worldnews</p>");
+  });
+  
+  $("#programmerdadjokes").click(function() {
+    console.log('programmerdadjokes');
+    $("#content").html("<p>programmerdadjokes</p>");
+  });
+
+  $("#upliftingnews").click(function() {
+    console.log('upliftingnews');
+    $("#content").html("<p>upliftingnews</p>");
+  });
+
+  $("#spacex").click(function() {
+    console.log('spacex');
+    $("#content").html("<p>spacex</p>");
+  });
+
+});
