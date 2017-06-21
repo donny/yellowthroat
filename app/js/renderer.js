8255f38b1eaaf1b5e77dc4da76b6c56539615409
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-06-22 08:10:28 +1000

    Generalise the function

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,52 +1,28 @@
 $(document).ready(function() {
+    $("#worldnews").click(() => handler("worldnews"));
+    $("#programmerdadjokes").click(() => handler("programmerdadjokes"));
+    $("#upliftingnews").click(() => handler("upliftingnews"));
+    $("#spacex").click(() => handler("spacex"));
+});
 
-    $("#worldnews").click(function() {
-        console.log('worldnews');
-        $("#content").html("<p>worldnews</p>");
+function handler(sub) {
+    console.log(sub);
+    $("#content").html("<p>" + sub + "</p>");
 
-
-        $.getJSON(
-                "http://www.reddit.com/r/worldnews.json?jsonp=?",
-                function foo(data) {
-                    $.each(
-                        data.data.children.slice(0, 10),
-                        function(i, post) {
-                            $("#content").append('<br>' + post.data.title);
-                            $("#content").append('<br>' + post.data.url);
-                            $("#content").append('<br>' + post.data.permalink);
-                            $("#content").append('<br>' + post.data.ups);
-                            $("#content").append('<br>' + post.data.downs);
-                            $("#content").append('<hr>');
-                        }
-                    )
+    $.getJSON(
+        "http://www.reddit.com/r/" + sub + ".json?jsonp=?",
+        function foo(data) {
+            $.each(
+                data.data.children.slice(0, 10),
+                function(i, post) {
+                    $("#content").append('<br>' + post.data.title);
+                    $("#content").append('<br>' + post.data.url);
+                    $("#content").append('<br>' + post.data.permalink);
+                    $("#content").append('<br>' + post.data.ups);
+                    $("#content").append('<br>' + post.data.downs);
+                    $("#content").append('<hr>');
                 }
             )
-            .success(function() {
-                alert("second success");
-            })
-            .error(function() {
-                alert("error");
-            })
-            .complete(function() {
-                alert("complete");
-            });
-
-
-    });
-
-    $("#programmerdadjokes").click(function() {
-        console.log('programmerdadjokes');
-        $("#content").html("<p>programmerdadjokes</p>");
-    });
-
-    $("#upliftingnews").click(function() {
-        console.log('upliftingnews');
-        $("#content").html("<p>upliftingnews</p>");
-    });
-
-    $("#spacex").click(function() {
-        console.log('spacex');
-        $("#content").html("<p>spacex</p>");
-    });
-
-});
+        }
+    );
+}
